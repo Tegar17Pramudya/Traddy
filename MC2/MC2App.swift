fb548b1f0b5e3bf93d46a7d9df41fccafd8238db
Tegar <tegar20ixc@gmail.com>
2023-05-28 13:44:01 +0700

    Initial Commit

---
+++ MC2/MC2App.swift
@@ -0,0 +1,17 @@
+//
+//  MC2App.swift
+//  MC2
+//
+//  Created by I Nyoman Tegar Pramudya on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
